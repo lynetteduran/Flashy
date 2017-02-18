891fa3106ed1621359c0a7ec2c1c85133f4ad0df
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2017-02-18 15:48:57 -0800

    adds user show function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,23 @@
 var db = require('../models');
 
-//GET /api/albums
+//GET ALL USERS ('/api/users')
 function index(req, res) {
   db.User.find({}, function(err, allUsers) {
     res.json(allUsers);
   });
 }
 
+//GET A USER ('/api/users/:userId')
+function show(req, res) {
+  db.User.findById(req.params.userId, function(err, foundUser) {
+    if (err) {console.log('ERROR getting user:' + req.params.userId)}
+    console.log('userController.show responding with', foundUser);
+    res.json(foundUser);
+  });
+}
+
 //EXPORTS PUBLIC METHODS
 module.exports = {
-  index: index
+  index: index,
+  show: show,
 }
