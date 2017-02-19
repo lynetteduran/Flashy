c0d925e65e0c58730567c03978d457f67a6f4b04
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2017-02-18 20:29:16 -0800

    adds user update function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,13 @@
 var db = require('../models');
 
-//GET ALL USERS ('/api/users')
+//GET ALL USERS @ ('/api/users')
 function index(req, res) {
   db.User.find({}, function(err, allUsers) {
     res.json(allUsers);
   });
 }
 
-//GET A USER ('/api/users/:userId')
+//GET A USER @ ('/api/users/:userId')
 function show(req, res) {
   db.User.findById(req.params.userId, function(err, foundUser) {
     if (err) {console.log('ERROR getting user:' + req.params.userId)}
@@ -16,7 +16,7 @@ function show(req, res) {
   });
 }
 
-//POST A USER('/api/users')
+//POST A USER @ ('/api/users')
 function create(req, res) {
   console.log('body', req.body);
   db.User.create(req.body, function(err, newUser) {
@@ -25,9 +25,24 @@ function create(req, res) {
   });
 }
 
+//UPDATE A USER @ ('/api/users')
+function update(req, res) {
+  console.log('updating user with data: ' + req.body);
+  db.User.findById(req.params.userId, function(err, foundUser) {
+    if(err) {console.log("Error updating user", err);}
+    foundUser.userName = req.body.userName;
+    foundUser.email = req.body.email;
+    foundUser.password = req.body.password;
+    foundUser.save(function(err, updatedUser) {
+      if(err) {console.log('saving updated user failed');}
+      res.json(updatedUser);
+    });
+  });
+}
 //EXPORTS PUBLIC METHODS
 module.exports = {
   index: index,
   show: show,
   create: create,
+  update: update,
 }
