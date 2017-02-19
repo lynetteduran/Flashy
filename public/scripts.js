fe31d442b1b7cbc99ecef91afde38b0b16d6dd95
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2017-02-18 22:06:26 -0800

    adds ajax call for user delete

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,22 @@
 console.log("Sanity Check: JS is working!");
 
+var allUsers = [];
+
 $(document).ready(function(){
+
+  $.ajax({
+    method: "GET",
+    url: "/api/users",
+    success: userIndexSuccess,
+    error: console.log("can't get users bro"),
+  });
 
   $('#userCreate').on('submit', function(e){
     e.preventDefault();
     var newUser = {
-                    userName : $('#userName').val(),
-                    email    : $('#email').val(),
-            };
+      userName : $('#userName').val(),
+      email    : $('#email').val(),
+    };
 
     $.ajax({
       method: "POST",
@@ -19,4 +28,31 @@ $(document).ready(function(){
       }
     });
   });
+
+  $('#userDelete').on('submit', function(e){
+    e.preventDefault();
+
+  });
 });
+
+function renderUsers(user){
+  $('#users').empty();
+
+  var usersHtml =
+  "      <fieldset>" +
+  "        <div data-user-id='" + user.userName "'>" +
+  "          <h4>User Name:</h4>" +
+  "          <div class='user-name'>" + user.userName + "</div>" +
+  "          <button id='userDelete'>Delete</button>" +
+  "        </div>"
+  "      </fieldset>";
+
+  $('#users').prepend(usersHtml);
+}
+
+function userIndexSuccess(json){
+  allUsers = json;
+  allUsers.forEach(function(user){
+    renderUsers(user);
+  });
+};
