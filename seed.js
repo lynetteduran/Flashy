e1c859e36bc01fe3eea39be5367376e3605743de
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2017-02-22 12:43:20 -0800

    adds sample decks to seed data file

--- seed.js
+++ seed.js
@@ -1,72 +1,151 @@
 var db = require('./models');
 
-var sampleUsers = [];
-sampleUsers.push({
-  userName: 'Sherri',
-  email: 'sherri@user.com',
-  password: 'Sherri'
-});
-sampleUsers.push({
-  userName: 'Chris',
-  email: 'chris@user.com',
-  password: 'Chris'
-});
-sampleUsers.push({
-  userName: 'Teddy',
-  email: 'teddy@user.com',
-  password: 'Teddy'
-});
-sampleUsers.push({
-  userName: 'Kenny',
-  email: 'kenny@user.com',
-  password: 'Kenny'
-});
-sampleUsers.push({
-  userName: 'Bill',
-  email: 'bill@user.com',
-  password: 'Bill'
-});
-sampleUsers.push({
-  userName: 'Natalia',
-  email: 'natalia@user.com',
-  password: 'Natalia'
-});
-sampleUsers.push({
-  userName: 'Lily',
-  email: 'lily@user.com',
-  password: 'Lily'
-});
-sampleUsers.push({
-  userName: 'Brandon',
-  email: 'brandon@user.com',
-  password: 'Brandon'
-});
-sampleUsers.push({
-  userName: 'Toby',
-  email: 'toby@user.com',
-  password: 'Toby'
-});
-sampleUsers.push({
-  userName: 'Ryan',
-  email: 'ryan@user.com',
-  password: 'Ryan'
-});
-sampleUsers.push({
-  userName: 'Alivia',
-  email: 'alivia@user.com',
-  password: 'Alivia'
-});
-sampleUsers.push({
-  userName: 'Lynette',
-  email: 'lynette@user.com',
-  password: 'Lynette'
-});
+var sampleUsers = [
+  {
+    userName: 'Sherri',
+    email: 'sherri@user.com',
+    password: 'Sherri'
+  },
+  {
+    userName: 'Chris',
+    email: 'chris@user.com',
+    password: 'Chris'
+  },
+  {
+    userName: 'Teddy',
+    email: 'teddy@user.com',
+    password: 'Teddy'
+  },
+  {
+    userName: 'Kenny',
+    email: 'kenny@user.com',
+    password: 'Kenny'
+  },
+  {
+    userName: 'Bill',
+    email: 'bill@user.com',
+    password: 'Bill'
+  },
+  {
+    userName: 'Natalia',
+    email: 'natalia@user.com',
+    password: 'Natalia'
+  },
+  {
+    userName: 'Lily',
+    email: 'lily@user.com',
+    password: 'Lily'
+  },
+  {
+    userName: 'Brandon',
+    email: 'brandon@user.com',
+    password: 'Brandon'
+  },
+  {
+    userName: 'Toby',
+    email: 'toby@user.com',
+    password: 'Toby'
+  },
+  {
+    userName: 'Ryan',
+    email: 'ryan@user.com',
+    password: 'Ryan'
+  },
+  {
+    userName: 'Alivia',
+    email: 'alivia@user.com',
+    password: 'Alivia'
+  },
+  {
+    userName: 'Lynette',
+    email: 'lynette@user.com',
+    password: 'Lynette'
+  }
+];
 
-db.User.remove({}, function(err, users) {
-  db.User.create(sampleUsers, function(err, users) {
-    if (err) {return console.log('ERROR', err);}
-    console.log("all users:", users);
-    console.log("created", users.length, "users");
-    process.exit();
+var sampleDecks = [
+  {
+    deckName: 'Geometry',
+    subject: 'Math'
+  },
+  {
+    deckName: 'Fractions',
+    subject: 'Math'
+  },
+  {
+    deckName: 'Impressionism',
+    subject: 'Art History'
+  },
+  {
+    deckName: 'Nouns',
+    subject: 'English'
+  },
+  {
+    deckName: 'JavaScript',
+    subject: 'Web Development'
+  },
+  {
+    deckName: 'Cells',
+    subject: 'Biology'
+  },
+  {
+    deckName: 'Electrons',
+    subject: 'Chemistry'
+  },
+  {
+    deckName: 'Gravity',
+    subject: 'Physics'
+  },
+  {
+    deckName: 'Parabolas',
+    subject: 'Algebra'
+  },
+  {
+    deckName: 'Angles',
+    subject: 'Geometry'
+  },
+  {
+    deckName: 'DNA',
+    subject: 'Genetics'
+  },
+  {
+    deckName: 'Streches',
+    subject: 'Physical Education'
+  },
+];
+
+db.Deck.remove({}, function(err, decks){
+  console.log('removes all decks');
+  db.Deck.create(sampleDecks, function(err, decks){
+    if (err){console.log(err);
+    return;
+    }
+    console.log('recreated all decks');
+    console.log('created', decks.length, "decks");
+
+    db.User.remove({}, function(err, users){
+      console.log('removed all users');
+      sampleUsers.forEach(function(userData){
+        var user = new db.User({
+          userName: userData.userName,
+          email: userData.userData,
+          password: userData.password,
+        });
+        db.Deck.findOne({name: userData.deck}, function(err, foundDeck){
+          console.log('found deck' + foundDeck.deckName + ' for user ' + user.userName);
+          if (err){
+            console.log(err);
+            return;
+          }
+          user.deck = foundDeck;
+          user.save(function(err, savedUser){
+            if (err){
+              return console.log(err);
+            }
+            console.log('saved' + savedUser.userName + "'s deck by name of " + foundDeck.deckName);
+          });
+        });
+      });
+    });
   });
 });
