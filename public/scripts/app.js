6878c7b10c6e04a8a3e77e3f3204d81d5512d262
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2017-02-25 07:31:19 -0800

    adds view/controller logic as outline for future functioning

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,67 @@
-//CLIENT-SIDE JAVASCRIPT
+//ANGULAR ROUTES
+
+angular
+  .module('flashy', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config($routeProvider, $locationProvider){
+  $routeProvider
+
+  .when('/', {
+    templateUrl: '/templates/flashy-home',
+    controllerAs: 'usersIndexCtrl',
+    controller: 'UsersIndexController'
+  })
+  .when('/signup', {
+    templateUrl: '/templates/signup',
+    controllerAs: 'usersIndexCtrl',
+    controller: 'UsersIndexController'
+  })
+  .when('/login', {
+    templateUrl: '/templates/login',
+    controllerAs: 'usersIndexCtrl',
+    controller: 'UsersIndexController'
+  })
+  .when('/users', {
+    templateUrl: '/templates/users/users-index',
+    controllerAs: 'usersIndexCtrl',
+    controller: 'UsersIndexController'
+  })
+  .when('/users/:id', {
+    templateUrl: '/templates/users/users-show',
+    controllerAs: 'usersShowCtrl',
+    controller: 'UsersShowController'
+  })
+  .when('/decks', {
+    templateUrl: '/templates/decks/decks-index',
+    controllerAs: 'decksIndexCtrl',
+    controller: 'DecksIndexController'
+  })
+  .when('/decks/:id', {
+    templateUrl: '/templates/decks/decks-show',
+    controllerAs: 'decksShowCtrl',
+    controller: 'DecksShowController'
+  })
+  .when('/decks/:id/choose-game', {
+    templateUrl: '/templates/game/choose',
+    controllerAs: 'decksShowCtrl',
+    controller: 'DecksShowController'
+  })
+  .when('/decks/:id/memory-game', {
+    templateUrl: '/templates/game/memory',
+    controllerAs: 'cardsIndexCtrl',
+    controller: 'CardsIndexController'
+  })
+  .when('/decks/:id/quiz-mode', {
+    templateUrl: '/templates/game/quiz',
+    controllerAs: 'cardsIndexCtrl',
+    controller: 'CardsIndexController'
+  })
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}
