caa553a01c6db3da1e6a711e75a5b303c509fcfb
Lynette Duran <lyndduran@gmail.com>
2017-03-03 13:21:43 -0800

    corrects spelling, comments out routes not yet in use

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,4 @@
 //ANGULAR ROUTES
-
 angular
   .module('flashy', ['ngRoute'])
   .config(config);
@@ -10,55 +9,55 @@ function config($routeProvider, $locationProvider){
   $routeProvider
 
   .when('/', {
-    templateUrl: '/templates/home/flashy-home',
+    templateUrl: '/templates/flashy-home',
     controllerAs: 'usersIndexCtrl',
     controller: 'UsersIndexController'
   })
   .when('/signup', {
-    templateUrl: '/templates/home/signup',
+    templateUrl: '/templates/signup',
     controllerAs: 'usersIndexCtrl',
     controller: 'UsersIndexController'
   })
-  .when('/login', {
-    templateUrl: '/templates/home/login',
-    controllerAs: 'usersIndexCtrl',
-    controller: 'UsersIndexController'
-  })
-  .when('/users', {
-    templateUrl: '/templates/users/users-index',
-    controllerAs: 'usersIndexCtrl',
-    controller: 'UsersIndexController'
-  })
+  // .when('/login', {
+  //   templateUrl: '/templates/login',
+  //   controllerAs: 'usersIndexCtrl',
+  //   controller: 'UsersIndexController'
+  // })
+  // .when('/users', {
+  //   templateUrl: '/templates/users-index',
+  //   controllerAs: 'usersIndexCtrl',
+  //   controller: 'UsersIndexController'
+  // })
   .when('/users/:id', {
-    templateUrl: '/templates/users/users-show',
+    templateUrl: '/templates/users-show',
     controllerAs: 'usersShowCtrl',
     controller: 'UsersShowController'
   })
-  .when('/decks', {
-    templateUrl: '/templates/decks/decks-index',
-    controllerAs: 'decksIndexCtrl',
-    controller: 'DecksIndexController'
-  })
-  .when('/decks/:id', {
-    templateUrl: '/templates/decks/decks-show',
-    controllerAs: 'decksShowCtrl',
-    controller: 'DecksShowController'
-  })
-  .when('/decks/:id/choose-game', {
-    templateUrl: '/templates/game/choose',
-    controllerAs: 'decksShowCtrl',
-    controller: 'DecksShowController'
-  })
-  .when('/decks/:id/memory-game', {
-    templateUrl: '/templates/game/memory',
-    controllerAs: 'cardsIndexCtrl',
-    controller: 'CardsIndexController'
-  })
-  .when('/decks/:id/quiz-mode', {
-    templateUrl: '/templates/game/quiz',
-    controllerAs: 'cardsIndexCtrl',
-    controller: 'CardsIndexController'
-  })
+  // .when('/decks', {
+  //   templateUrl: '/templates/decks-index',
+  //   controllerAs: 'decksIndexCtrl',
+  //   controller: 'DecksIndexController'
+  // })
+  // .when('/decks/:id', {
+  //   templateUrl: '/templates/decks-show',
+  //   controllerAs: 'decksShowCtrl',
+  //   controller: 'DecksShowController'
+  // })
+  // .when('/decks/:id/choose-game', {
+  //   templateUrl: '/templates/choose',
+  //   controllerAs: 'decksShowCtrl',
+  //   controller: 'DecksShowController'
+  // })
+  // .when('/decks/:id/memory-game', {
+  //   templateUrl: '/templates/memory',
+  //   controllerAs : 'cardsIndexCtrl',
+  //   controller: 'CardsIndexController'
+  // })
+  // .when('/decks/:id/quiz-mode', {
+  //   templateUrl: '/templates/quiz',
+  //   controllerAs: 'cardsIndexCtrl',
+  //   controller: 'CardsIndexController'
+  // })
 
   $locationProvider.html5Mode({
     enabled: true,
